744fbe4d10bb04a1490948795f284b72a0bea026
skarlsson <svante.karlsson@csi.se>
2016-01-20 20:59:37 +0100

    typo

--- linux_rebuild_csi.sh
+++ linux_rebuild_csi.sh
@@ -19,7 +19,17 @@ git pull
 bash -e build_linux.sh
 cd ..
 
-cd csi-http
+cd csi-http-common
+git pull
+bash -e build_linux.sh
+cd ..
+
+cd csi-http-client
+git pull
+bash -e build_linux.sh
+cd ..
+
+cd csi-http-server
 git pull
 bash -e build_linux.sh
 cd ..
